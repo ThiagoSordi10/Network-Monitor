dbf5d4d86478ed42c4a1c9342da4d7713d1a20b7
Thiago Sordi <thiagosordi10@gmail.com>
2019-09-30 14:54:55 -0300

    Update spark.py

--- Spark-docker/spark.py
+++ Spark-docker/spark.py
@@ -3,41 +3,50 @@ from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 from cassandra_helper import *
 import docker
+import json
+
+clusterIPs = ["172.24.0.2"]
 
 def sendPartition(iter):
     # ConnectionPool is a static, lazily initialized pool of connections
     #connection = ConnectionPool.getConnection()
+    ConnectDB( clusterIPs )
+    print("Starting ...")
     for record in iter:
         #connection.send(record)
         print(record)
+	record = json.loads(record)
         if 'conn' in record:
-                record['conn'].update(identification = time.time())
+                #record['conn'].update(identification = time.time())
                 insert_connection(record['conn'])
         elif 'dns' in record:
-                record['dns'].update(identification = time.time())
+                #record['dns'].update(identification = time.time())
                 insert_dns(record['dns'])
         elif 'dhcp' in record:
-                record['dhcp'].update(identification = time.time())
+                #record['dhcp'].update(identification = time.time())
                 insert_dhcp(record['dhcp'])
         elif 'ssh' in record:
-                record['ssh'].update(identification = time.time())
+                #record['ssh'].update(identification = time.time())
                 insert_ssh(record['ssh'])
         elif 'http' in record:
-                record['http'].update(identification = time.time())
+                #record['http'].update(identification = time.time())
                 insert_http(record['http'])
     # return to the pool for future reuse
     #ConnectionPool.returnConnection(connection)
+#dstream.foreachRDD(lambda rdd: rdd.foreachPartition(sendPartition))
 
 # get docker containers from cassandra
 client = docker.from_env()
-clusterIPs = []
+
+
+'''
+print(client.containers.list())
 for container in client.containers.list(filters={'name': "cassandra"}):
         cnet = next(iter(vars(container)["attrs"]["NetworkSettings"]["Networks"].values()))
         print('Found cassandra node ' + container.name)
-        clusterIPs.append(cnet['IPAddress'])
+        clusterIPs.append(cnet['IPAddress'])'''
 
-ConnectDB( clusterIPs )
-print("Starting ...")
+
 
 sc = SparkContext("local[2]", appName="PythonSparkStreamingKafka")
 sc.setLogLevel("WARN")
@@ -53,5 +62,3 @@ ssc.start()
 ssc.awaitTermination()
 
 
-
-#dstream.foreachRDD(lambda rdd: rdd.foreachPartition(sendPartition))
